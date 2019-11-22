35933444dc1b7b8fe8bb66837e93703c9cc5b3ea
modrobert <modrobert@gmail.com>
2019-11-22 14:32:47 +0700

    defined some constants

--- blake2bcmd.c
+++ blake2bcmd.c
@@ -8,29 +8,32 @@
 
 #include "blake2b.h"
 
+#define BUFSIZE 1 /* Read buffer size. */
+#define MDSIZE 64 /* Message digest 512 bit. */
+
 int main()
 {
- uint8_t md[64];
+ uint8_t md[MDSIZE];
  blake2b_ctx ctx;
 
- uint8_t buf[1];
+ uint8_t buf[BUFSIZE];
  size_t i;
 
- /* Init blake2b, set 512 bit (8 * 64). */
- if (blake2b_init(&ctx, 64, NULL, 0))
+ /* Initialize blake2b hashing context. */
+ if (blake2b_init(&ctx, MDSIZE, NULL, 0))
   return -1;
 
  /* Read from stdin. */
- while (read(STDIN_FILENO, buf, sizeof(buf)) > 0)
+ while (read(STDIN_FILENO, buf, BUFSIZE) > 0)
  {
-  blake2b_update(&ctx, buf, 1);
+  blake2b_update(&ctx, buf, BUFSIZE);
  }
 
  /* Final round. */
  blake2b_final(&ctx, md);
 
  /* Print result hash. */
- for (i = 0; i < 64; i++)
+ for (i = 0; i < MDSIZE; i++)
  {
   printf("%02x", md[i]);
  }
